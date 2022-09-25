7e268deb01616cd678b3921741886fd1b00b3aac
Неугодников Сергей Игоревич <59767230+RexarSergey@users.noreply.github.com>
2022-09-26 01:53:13 +0500

    feature-01 | HW3 (#1)
    * added dz3 realization with additional task
    
    * naming, color, indent fixed
    
    * added destructuring
    
    * code refactor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {MessageCard} from "./components/message-card/message-card";
+import mockData from "./assets/mock-data"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        // Mapping elements form mock-data
+        <div className="App">
+            {mockData.map(({title, text, currentLikes}) => <MessageCard
+                title={title}
+                text={text}
+                currentLikes={currentLikes}
+            />)}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/message-card/message-card.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from "react"
+import s from "./message-card.module.css"
+
+
+export function MessageCard({title, text, currentLikes}) {
+    // useState hook for like counter
+    const [counter, setCounter] = useState(currentLikes)
+    // useState hook for like button state
+    const [state, setState] = useState(false)
+
+    // Customize info about current likes
+    const likeInfo = `Current likes: ${counter}`
+
+    // Function, that change number of likes and button state with its color
+    const push = () => {
+        if (state) {
+            setCounter(oldCounter => oldCounter - 1)
+            setState(false)
+            return
+        }
+
+        setCounter(oldCounter => oldCounter + 1)
+        setState(true)
+    }
+
+    // Card realization
+    return (
+            <div className={s.card}>
+                <h1>{title}</h1>
+                <p className={s.content}>{text}</p>
+                <p className={s.likesInfo}>{likeInfo}</p>
+                <div className={s.buttonPosition}>
+                    <div
+                        className={s.button}
+                        style={{
+                            backgroundColor: state ? '#ff0000' : '#808080'
+                        }}
+                        onClick={push}>Like
+                    </div>
+                </div>
+            </div>
+    )
+}
