b05d37e61e8b2677e6bb3a9776be5dc5e265226b
RexarSergey <zizassassinziz@gmail.com>
2022-10-25 10:19:03 +0300

    final version

--- src/components/async-cards/async-realization/async-realization.jsx
+++ src/components/async-cards/async-realization/async-realization.jsx
@@ -4,30 +4,12 @@ import {MessageCard} from "../async-message-card/async-message-card";
 import {InputMessageCard} from "../../input-forms/input-message-card/input-message-card"
 
 
-const initialValues = {
-    articleId: Math.floor(Math.random() * 100),
-    title: "",
-    text: "",
-    currentLikes: 0,
-    commentsCount: 0
-}
-
 export function AsyncRealization() {
     const [data, setData] = useState(null)
-    const [line, setLine] = useState(initialValues)
 
     useEffect(() => {
         getArticles().then(fetchedData => setData(fetchedData))
     }, [])
-
-    const onChange = event => {
-        const {name, value} = event.target
-        setLine({...line, [name]: value})
-    }
-
-    const pushLine = () => {
-        setData([...data, line])
-    }
 
     return data
         ? <div> {data.map(({articleId, title, text, currentLikes, commentsCount}) =>
@@ -39,10 +21,8 @@ export function AsyncRealization() {
                 commentsCount={commentsCount}
             />)}
             <InputMessageCard
-                data={line}
-                onChange={onChange}
+                setData={setData}
             />
-            <button onClick={pushLine}>Push</button>
         </div>
         : <div> Loading DATA... </div>
 }
--- src/components/input-forms/input-message-card/input-message-card.jsx
+++ src/components/input-forms/input-message-card/input-message-card.jsx
@@ -1,31 +1,74 @@
-import React from "react";
+import React, {useState} from "react";
 
+const initialValues = {
+    articleId: Math.floor(Math.random() * 100),
+    title: "",
+    text: "",
+    currentLikes: 0,
+    commentsCount: 0
+}
 
-export const InputMessageCard = ({data, onChange}) => (
-    <form className="Form">
-        <input
-            value={data.title}
-            onChange={onChange}
-            name="title"
-            placeholder={"Title"}
-        />
-        <input
-            value={data.text}
-            onChange={onChange}
-            name="text"
-            placeholder={"Text"}
-        />
-        <input
-            value={data.currentLikes}
-            onChange={onChange}
-            name="currentLikes"
-            placeholder={"Current Likes"}
-        />
-        <input
-            value={data.commentsCount}
-            onChange={onChange}
-            name="commentsCount"
-            placeholder={"Comments Count"}
-        />
-    </form>
-)
+export function InputMessageCard({setData}) {
+
+    const [line, setLine] = useState(initialValues)
+    const [messageTitle, setMessageTitle] = useState(initialValues.title)
+    const [messageText, setMessageText] = useState(initialValues.text)
+    const [messageLikes, setMessageLikes] = useState(initialValues.currentLikes)
+    const [messageComments, setMessageComments] = useState(initialValues.commentsCount)
+
+    const onTitleChange = event => {
+        const {value} = event.target
+        setMessageTitle(value)
+        setLine({...line, ["title"]:value})
+    }
+
+    const onTextChange = event => {
+        const {value} = event.target
+        setMessageText(value)
+        setLine({...line, ["text"]:value})
+    }
+
+    const onLikesChange = event => {
+        const {value} = event.target
+        setMessageLikes(value)
+        setLine({...line, ["currentLikes"]:value})
+    }
+
+    const onCommentsChange = event => {
+        const {value} = event.target
+        setMessageComments(value)
+        setLine({...line, ["commentsCount"]:value})
+    }
+
+    const pushLine = () => {
+        setData(oldData => [...oldData, line])
+    }
+
+    return (
+        <>
+            <form className="Form">
+                <input
+                    value={messageTitle}
+                    onChange={onTitleChange}
+                    placeholder={"Title"}
+                />
+                <input
+                    value={messageText}
+                    onChange={onTextChange}
+                    placeholder={"Text"}
+                />
+                <input
+                    value={messageLikes}
+                    onChange={onLikesChange}
+                    placeholder={"Current Likes"}
+                />
+                <input
+                    value={messageComments}
+                    onChange={onCommentsChange}
+                    placeholder={"Comments Count"}
+                />
+            </form>
+            <button onClick={pushLine}>Push</button>
+        </>
+    )
+}
